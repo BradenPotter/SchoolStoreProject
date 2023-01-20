e82af56de7590d0df40cc856787e0eb786769bee
BradenPotter <braden.potter05@gmail.com>
2023-01-20 14:40:25 -0500

    added folder and new script

--- TestLive/SchoolStoreProject/Scripts/sidenavscript.js
+++ TestLive/SchoolStoreProject/Scripts/sidenavscript.js
@@ -1,27 +1,5 @@
-var dog = 0;
-var Counter;
-var cat = "hello";
 var yesorno = 0;
-document.getElementById("demo").innerHTML = dog;
-
-
-function myFunction(apple, carrot) {
-    console.log("hello");
-    if (apple === 'up') {
-      dog = document.getElementById(carrot).innerHTML
-      dog++;
-    } else if (dog !== 0) {
-      dog = document.getElementById(carrot).innerHTML
-    dog--;
-    }
-    document.getElementById(carrot).innerHTML = dog;
-  }
-
-  function changePage(page) {
-    location.href= page;
-  }
-
-  function changeNav() {
+function changeNav() {
     if (yesorno === 1) {
     document.getElementById("mySidenav").style.width = "0px";
     document.getElementById("contentContainer").style.marginRight = "0px";
