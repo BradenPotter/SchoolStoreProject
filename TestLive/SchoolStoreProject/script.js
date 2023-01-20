9e4aa12fdc411541c1102a2c51ddc9e451abab1a
BradenPotter <braden.potter05@gmail.com>
2023-01-20 14:04:39 -0500

    added a side bar

--- TestLive/SchoolStoreProject/script.js
+++ TestLive/SchoolStoreProject/script.js
@@ -1,6 +1,8 @@
 var dog = 0;
 var Counter;
 var cat = "hello";
+var yesorno = 0;
+document.getElementById("demo").innerHTML = dog;
 
 
 function myFunction(apple, carrot) {
@@ -15,3 +17,22 @@ function myFunction(apple, carrot) {
     document.getElementById(carrot).innerHTML = dog;
   }
 
+  function changePage(page) {
+    location.href= page;
+  }
+
+  function changeNav() {
+    if (yesorno === 1) {
+    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("contentContainer").style.marginRight = "0px";
+    document.getElementById("header").style.marginRight = "0px";
+    document.getElementById("footer").style.marginRight = "0px";
+    yesorno = 0;
+    } else {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("contentContainer").style.marginRight = "250px";
+    document.getElementById("header").style.marginRight = "250px";
+    document.getElementById("footer").style.marginRight = "250px";
+    yesorno = 1;
+    }
+  }
