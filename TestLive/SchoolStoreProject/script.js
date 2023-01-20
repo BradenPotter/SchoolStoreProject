f6f19b26aafbe2375ac6f743c6464a4ffceb976d
micspinks <mspinks2005@gmail.com>
2023-01-20 11:51:35 -0500

    Workingindividual

--- TestLive/SchoolStoreProject/script.js
+++ TestLive/SchoolStoreProject/script.js
@@ -1,13 +1,15 @@
 var dog = 0;
 var Counter;
 var cat = "hello";
-document.getElementById("demo").innerHTML = dog;
 
 
 function myFunction(apple, carrot) {
+    console.log("hello");
     if (apple === 'up') {
+      dog = document.getElementById(carrot).innerHTML
       dog++;
     } else if (dog !== 0) {
+      dog = document.getElementById(carrot).innerHTML
     dog--;
     }
     document.getElementById(carrot).innerHTML = dog;
