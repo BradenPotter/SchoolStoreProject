9030d01105c187b0e62f48d138f7aca0d5d0b5ff
micspinks <mspinks2005@gmail.com>
2023-01-23 12:36:39 -0500

    WorkingOrderpage

--- TestLive/SchoolStoreProject/Scripts/script.js
+++ TestLive/SchoolStoreProject/Scripts/script.js
@@ -1,21 +1,41 @@
-var dog = 0;
+var Count = 0;
 var Counter;
-var cat = "hello";
-//document.getElementById("demo").innerHTML = dog;
+var CurrentCount;
+var ProductLogged;
+var Para;
+var Price;
+var Replace; 
+var Cost;
 
-
-function myFunction(apple, carrot) {
+function myFunction(CounterDirection, SelectedCounter) {
     console.log("hello");
-    if (apple === 'up') {
-      dog = document.getElementById(carrot).innerHTML
-      dog++;
-    } else if (dog !== 0) {
-      dog = document.getElementById(carrot).innerHTML
-    dog--;
+    if (CounterDirection === 'up') {
+      Count = document.getElementById(SelectedCounter).innerHTML
+      Count++;
+    } else if (Count !== 0) {
+      Count = document.getElementById(SelectedCounter).innerHTML
+    Count--;
     }
-    document.getElementById(carrot).innerHTML = dog;
+    document.getElementById(SelectedCounter).innerHTML = Count;
   }
 
   function changePage(page) {
     location.href= page;
   }
+  function BtnFunction(Product, SelectedCounter, CurrentPrice) {
+    CurrentCount = document.getElementById(SelectedCounter).innerHTML;
+    console.log(CurrentCount)
+    if (CurrentCount != 0) {
+      Para = document.createElement("p");
+      Price = document.getElementById(CurrentPrice).innerHTML
+      Replace = Price.replace("$", "");
+      console.log(Replace);
+      Cost = Replace*CurrentCount;
+      Para.innerText = CurrentCount + " " + Product + ": "  + Cost ;
+      document.getElementById("TestWords").appendChild(Para);
+
+    }
+  }
+
+
+
